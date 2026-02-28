a23794dfd8050fd05dddf8aacabaf697a7f1f2a9
Rama Krishnam Raju Kalidindi <krishna@Ramas-Mac-Studio.attlocal.net>
2026-02-27 21:15:04 -0500

    Updated setup with basic find and oil

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,15 @@ local map = vim.keymap.set
 map('n', '<leader>w', '<cmd>write<cr>', { desc = "Save" })
 map('n', '<leader>q', '<cmd>quit<cr>', { desc = "Quit" })
 
-map("n", "<leader>ff", function() pick.builtin.files() end, { desc = "Find files" })
-map("n", "<leader>fg", function() pick.builtin.grep_live() end, { desc = "Live grep" })
-map("n", "<leader>fb", function() pick.builtin.buffers() end, { desc = "Buffers" })
-map("n", "<leader>fh", function() pick.builtin.help() end, { desc = "Help tags" })
+local builtin = require("telescope.builtin")
+
+map("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+map("n", "<leader>fg", builtin.live_grep, { desc = "Find Grep" })
+map("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
+map("n", "<leader>fr", builtin.oldfiles, { desc = "Find Old Files" })
+map("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = "Current Buffer Fuzzy Find" })
+
+map("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Oil" })
+map("n", "<leader>o", function() 
+  require("oil").open_float()
+end, {})
---
+++ lua/config/lazy.lua
@@ -0,0 +1,29 @@
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("oil").setup({
+        default_file_explorer = true,
+      })
+    end,
+  },
+})
